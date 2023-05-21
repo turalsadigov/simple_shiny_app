10800fdde1734b31074cc6a62d52df52c4e8e0c4
Tural Sadigov <turalsadikov@gmail.com>
2023-05-20 17:51:06 -0700

    gite gonderirem, connect'e gedecekmi deye

--- app/app.R
+++ app/app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Git'e gonderdim, ozu connect'e aldi"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
